7f434b515e1ad4c2a43eff5f6ef23173bff0d2ea
Navneet <mailsharmanavneet@gmail.com>
2024-02-24 23:27:49 +0530

    user input in array

--- inputfromuser.c
+++ inputfromuser.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    printf("enter a integer: a ");
-    scanf("%d",&a);
-    printf("a = %d",a);
+    int arr[5];
+    printf("enter five integers.\n");
+    for (int i=0; i<=4; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("You entered:  %d, %d, %d, %d, %d",arr[0],arr[1],arr[2],arr[3],arr[4]);
     return 0;
 }
