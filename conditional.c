dc06db0687205caf6a7b95a4cbca82d4f0466e50
Navneet <mailsharmanavneet@gmail.com>
2024-02-26 23:35:10 +0530

    conditional statement in c

---
+++ conditional.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter first integer: ");
+    scanf("%d",&a);
+    printf("enter second integer: ");
+    scanf("%d",&b);
+    if (a>b)
+    {
+    printf("first number is greater then second: ");
+    }
+    // printf("sum of first and second integer ia: %d",sum);
+   return 0; 
+}
