48edf3c6184f064c3fd29f7f7ce4af34bad56008
Navneet <mailsharmanavneet@gmail.com>
2024-02-26 23:46:07 +0530

    small change

--- inputfromuser.c
+++ inputfromuser.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int arr[5];
-    printf("enter five integers.\n");
-    for (int i=0; i<=4; i++)
+    int arr[8];
+    printf("enter eight integers.\n");
+    for (int i=0; i<=7; i++)
     {
         scanf("%d",&arr[i]);
     }
-    printf("You entered:  %d, %d, %d, %d, %d",arr[0],arr[1],arr[2],arr[3],arr[4]);
+    printf("You entered:  %d, %d, %d, %d, %d, %d, %d, %d",arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7]);
     return 0;
 }
